4fc6f8616ec28b2f57919e83643c349f530b9f90
bayedamediop <bayedame1823@gmail.com>
2021-07-29 09:35:53 +0000

    button rechercher

--- salarie.php
+++ salarie.php
@@ -108,10 +108,40 @@ $reponse = $bdd->query('SELECT * FROM salarie ORDER BY idSalarie ASC');
 
 
 <script>
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
 
+$(document).ready(function() {
+    var table = $('#example').DataTable( {
+        scrollY:        "500px",
+        scrollX:        true,
+        scrollCollapse: true,
+        paging:         true,
+		"lengthChange": false,
+
+	
+		
+		"oLanguage": {
+                   "sProcessing": "Traitement en cours...",
+                   "sSearch": "Rechercher&nbsp;:",
+                   "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
+                   "sInfo": "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+                   "sInfoEmpty": "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+                   "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+                   "sInfoPostFix": "",
+                   "sLoadingRecords": "Chargement en cours...",
+                   "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
+                   "sEmptyTable": "Aucune donn&eacute;e disponible dans le tableau",
+                   "oPaginate": {
+                       "sFirst": "Premier&nbsp;&nbsp;",
+                       "sPrevious": "Pr&eacute;c&eacute;dent&nbsp;&nbsp;",
+                       "sNext": "Suivant",
+                       "sLast": "&nbsp;&nbsp;Dernier"
+		}, },
+		
+	
+    } );
+	
+		
+} );
 </script>
 </body>
 </html>
